e08c2800197f6b8acc3db1912af4da3c1dd7ecfd
ajupton <theandyshow@gmail.com>
2018-09-05 10:37:20 -0700

    Update cultural transmission functions

--- R/cultural_transmission.R
+++ R/cultural_transmission.R
@@ -5,24 +5,29 @@
 # or spatially/temporally bounded location of artifact provencance. This variable is used
 # to group the artifacts. 
 
-# Refer to the "Networks of Interaction through cultural transmission rmarkdown document to 
+# Refer to the "Networks of Interaction through cultural transmission" rmarkdown document to 
 # see how these functions are applied. 
 
 library(tidyverse)
 
-#number of vessels
+# Length with removing missing values
+my_length <- function(x){
+  sum(!is.na(x))
+}
+
+# Number of vessels
 n_vessels <- function(x){
   x %>%
     summarise_all(my_length)
 }
 
-#function to calculate number of vessels by site
+# Function to calculate number of vessels by site
 v_vessels_by_site <- function(x){
   x %>% group_by(Site) %>% 
     summarise_all(my_length)
 }
 
-#function to return the critical.r
+# Function to return the critical.r
 critical_r <- function(n, alpha = 0.05) {
   df <- n - 2
   critical.t <- qt(alpha/2, df, lower.tail = F)
@@ -30,46 +35,41 @@ critical_r <- function(n, alpha = 0.05) {
   return(critical.r)
 }
 
-#function to check if site-attribute combination reached n-crit or replace with 0
+# Function to check if site-attribute combination reached n-crit or replace with 0
 crit_ifelse <- function(x){
   sum(ifelse(x >= 6, 1, 0))
 }
 
-#length with removing missing values
-my_length <- function(x){
-  sum(!is.na(x))
-}
-
-#Unbiased estimator of coefficient of variation
+# Unbiased estimator of coefficient of variation
 my_cv <- function(x){
   (sd(x, na.rm = TRUE)/mean(x, na.rm = TRUE)) * (1 + (1/(4*length(x[!is.na(x)]))))
 }
 
-#Standard Deviation to remove missing values
+# Standard Deviation to remove missing values
 my_sd <- function(x){
   sd(x, na.rm = TRUE)
 }
 
-#Mean function to remove missing values
+# Mean function to remove missing values
 my_mean <- function(x){
   mean(x, na.rm = TRUE)
 }
 
-#Variation of Variation (VOV)
+# Variation of Variation (VOV)
 VOV <- function(x){x %>%
     group_by(Site) %>%
     summarise_all(my_cv) %>%
     summarise_all(my_cv)
 }
 
-#Variation of the mean (VOM)
+# Variation of the mean (VOM)
 VOM <- function(x){x %>%
     group_by(Site) %>%
     summarise_all(my_mean) %>%
     summarise_all(my_cv)
 }
 
-#Average variation (AV)
+# Average variation (AV)
 AV <- function(x){x %>% 
     group_by(Site) %>%
     summarise_all(my_cv) %>%
